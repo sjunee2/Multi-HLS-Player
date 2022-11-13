f7bd0ade5405e7b98a41f1223542ee7db85a9a20
SJ Lee <tx0203@gmail.com>
2022-11-13 20:51:43 +0900

    Map Url

--- multi-hls-player/components/sidedrawer.tsx
+++ multi-hls-player/components/sidedrawer.tsx
@@ -1,14 +1,14 @@
-export default function Sidedrawer({ show }:{
+export default function Sidedrawer({ children, show }:{
+  children: React.ReactNode,
   show: boolean,
 }) {
   return (
-    <>
-      {show && (
-        <h1 className='text-sky-400'>
-          testdrawer
-        </h1>
-      )
-      }
-    </>
+    <nav className={"fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out"
+    + (show ? "transition-opacity opacity-100 duration-500 translate-x-0" : "transition-all delay-500 opacity-0 translate-x-full")}>
+      <ul className="w-full flex flex-col text-white px-5 py-4 space-y-4">
+        {children}
+      </ul>
+      testdrawer
+    </nav>
   )
 }
