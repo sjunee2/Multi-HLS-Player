80afab3f33fbfe514a257fc51843022070b0d79b
SJ Lee <tx0203@gmail.com>
2022-11-23 02:23:59 +0900

    Add delete button in each urlcase

--- multi-hls-player/components/urlcase.tsx
+++ multi-hls-player/components/urlcase.tsx
@@ -1,5 +1,15 @@
-export default function Urlcase({ url } : { url: string } ) {
+export default function Urlcase({ url, urlList, setUrlList } : {
+  url: string,
+  urlList: string[],
+  setUrlList: React.Dispatch<React.SetStateAction<string[]>>
+}) {
   return (
-    <div className="flex">{url}</div>
+    <div>
+      <div className="flex">{url}</div>
+      <button className= "px-3 py-2 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5" 
+      onClick={() => {
+        setUrlList(urlList => urlList.filter(item => item !== url));
+      }}> Delete </button>
+    </div>
   )
 }
